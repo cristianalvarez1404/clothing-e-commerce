7603252ddc97ab9214f0f724a80ab4e1c4408695
CristianAlvarez <cristianandres1496@gmail.com>
2024-01-13 21:22:49 -0500

    creating products page

--- front/index.js
+++ front/index.js
@@ -1,1 +1,16 @@
 console.log("Hello World");
+
+const num = "4";
+
+const num2 = parseInt(num);
+
+const userNumber = window.prompt("Write your number");
+
+try {
+} catch (err) {}
+
+const password = "123456+";
+
+const status = "ACTIVE";
+
+const functionX = () => "hola";
